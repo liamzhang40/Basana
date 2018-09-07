743dc04b96fbc50a3f323869878c145dbcc29798
Liangtian Zhang <lzhang40@binghamton.edu>
2018-09-07 17:41:35 -0400

    added comment dropdown button

--- frontend/components/comment/comment_index_item.jsx
+++ frontend/components/comment/comment_index_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TeamMemberIndexItem from '../team/team_member_index_item';
+import DropdownButton from '../button/dropdown_button';
 
 class CommentIndexItem extends React.Component {
 
@@ -12,6 +13,9 @@ class CommentIndexItem extends React.Component {
           <div>{ comment.author.name }</div>
           <div>{ comment.content }</div>
         </div>
+        <DropdownButton
+          buttonStyle={() => <svg viewBox="0 0 32 32"><path d="M16,22.5c-0.3,0-0.7-0.1-0.9-0.3l-11-9c-0.6-0.5-0.7-1.5-0.2-2.1c0.5-0.6,1.5-0.7,2.1-0.2L16,19.1l10-8.2c0.6-0.5,1.6-0.4,2.1,0.2c0.5,0.6,0.4,1.6-0.2,2.1l-11,9C16.7,22.4,16.3,22.5,16,22.5z"></path></svg>}
+          type="comment"/>
       </div>
     );
   }
