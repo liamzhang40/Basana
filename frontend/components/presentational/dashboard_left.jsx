2b817d0edbcf2027b39a8d2a05e34d7a2e9644b7
Liangtian Zhang <lzhang40@binghamton.edu>
2018-10-28 20:28:10 -0400

    changed dashboard left to functional component

--- frontend/components/presentational/dashboard_left.jsx
+++ frontend/components/presentational/dashboard_left.jsx
@@ -3,26 +3,20 @@ import { Route } from 'react-router-dom';
 import ProjectIndexContainer from '../project/project_index_container';
 import TeamMemberIndexContainer from '../team/team_member_index_container';
 
-class DashboardLeft extends React.Component {
-    constructor() {
-        super();
-    }
-
-    render() {
-        return (
-            <section className="dashboard-left">
-                <nav className='dashboard-left-content'>
-                    <div className='dashboard-title-section'>  
-                        <div>
-                            <h1>Basana</h1>
-                        </div>
+const DashboardLeft = () => {
+    return (
+        <section className="dashboard-left">
+            <nav className='dashboard-left-content'>
+                <div className='dashboard-title-section'>  
+                    <div>
+                        <h1>Basana</h1>
                     </div>
-                    <Route path='/dashboard/teams/:teamId' component={TeamMemberIndexContainer} />
-                    <Route path='/dashboard/teams/:teamId' component={ProjectIndexContainer} />
-                </nav>
-            </section>
-        );
-    }
-}
+                </div>
+                <Route path='/dashboard/teams/:teamId' component={TeamMemberIndexContainer} />
+                <Route path='/dashboard/teams/:teamId' component={ProjectIndexContainer} />
+            </nav>
+        </section>
+    );
+};
 
 export default DashboardLeft;
