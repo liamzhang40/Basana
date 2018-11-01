cdc7407dbac5f1fccdff8683ee5a9ae163d6f0a0
Liangtian Zhang <lzhang40@binghamton.edu>
2018-11-01 01:15:55 -0400

    added task completion button

--- frontend/components/calendar/calendar_task_index.jsx
+++ frontend/components/calendar/calendar_task_index.jsx
@@ -5,6 +5,7 @@ import CalendarTaskIndexItem from './calendar_task_index_item';
 const mapStateToProps = (state, ownProps) => {
     const tasks = Object.values(state.entities.tasks)
     .filter(task => (
+        task.due_date && 
         parseInt(task.due_date.slice(8)) === ownProps.date &&
         parseInt(task.due_date.slice(5, 7)) === ownProps.month + 1 &&
         parseInt(task.due_date.slice(0, 4)) === ownProps.year
--- frontend/components/calendar/calendar_task_index_item.jsx
+++ frontend/components/calendar/calendar_task_index_item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import TeamMemberIndexItem from '../team/team_member_index_item';
+import TaskCompletionButton from '../button/task_completion_button';
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -10,14 +11,40 @@ const mapStateToProps = (state, ownProps) => {
     };
 };
 
-const CalendarTaskIndexItem = ({ task, assignee }) => {
-    return (
-        <div className="calendar-task-index-item">
-            <TeamMemberIndexItem 
-                member={assignee}/>
-            <div className="calendar-task-index-item-name">{task.name}</div>
-        </div>
-    );
+class CalendarTaskIndexItem extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            visible: false
+        };
+
+        this.handleMouse = this.handleMouse.bind(this);
+    }
+
+    handleMouse() {
+        this.setState({visible: !this.state.visible});
+    }
+
+    render() {
+        const { assignee, task } = this.props;
+        return (
+            <div className="calendar-task-index-item"
+                onMouseEnter={ this.handleMouse }
+                onMouseLeave={ this.handleMouse }>
+                { this.state.visible &&
+                    <div 
+                        className="button-container"
+                        ref={node => { this.node = node; }}>
+                        <TaskCompletionButton task={task}/>
+                    </div>}
+                <TeamMemberIndexItem 
+                    member={assignee}/>
+                <div className="calendar-task-index-item-name">{task.name}</div>
+            </div>
+        );
+
+    }
 };
 
 export default connect(mapStateToProps)(CalendarTaskIndexItem);
